20facb9cc6b1b72b71aa36eb696493dc8daa71e9
kevinmalats <ken_1721@hotmail.es>
2019-02-26 12:54:53 -0500

    primera aplicacion

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+@Component({
+selector: 'app-home',
+templateUrl: './home.component.html',
+styleUrls: ['./home.component.scss']
+})
+export class HomeComponent implements OnInit {
+ logros : ILogro[];
+ titulo  : string = 'Bienvenidos';
+ constructor() { }
+ ngOnInit() {
+ this.logros = this.getLogros();
+ }
+ getLogros() : ILogro[]{
+  return [{
+   id:1,
+   title:"Campeonato Mundial de ajedrez",
+   description:"Los mejores jugadores enfrentandose"
+  }, {
+   id:2,
+   title:"Super estreno de Avengers 4",
+   description:"Thanos derrota a todos los avengers"
+  }, {
+   id:3,
+   title:"Bhomeian Rapsody",
+   description:"Un exito total"
+  }
+ ]
+ }
+}
+interface ILogro{
+  id : number;
+  title : string;
+  description ?: string;
+}
